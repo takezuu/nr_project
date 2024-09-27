29c8c11eaf24155bb53ebd94da1f66331c3100d8
NikitaPRV <nikitaprv@oxygen>
2024-09-27 22:44:46 +0300

    added Move api

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
 app.mount("/", StaticFiles(directory="front", html=True), name="front")
+
+
+class MoveReq(BaseModel):
+    direction: str
+    cellNum: int
+
+
+@app.post("/move")
+async def move(move: MoveReq):
+    if move.direction == "right" and move.cellNum + 1 > move.cellNum:
+        return {"Message": f"You can move cellNum = {move.cellNum + 1}"}
+    else:
+        return {"Status": 500, "Message": "Wrong move"}
