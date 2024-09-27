7ed8e65230c93084d1eea4196db04c58d526960f
NikitaPRV <nikitaprv@oxygen>
2024-09-28 00:55:39 +0300

    changed back main.py; added favicon.ico

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from starlette.staticfiles import StaticFiles
+from fastapi import status
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="front", html=True), name="front")
+app.mount("/static", StaticFiles(directory="front"), name="static")
+
+
+@app.get("/")
+async def main():
+    return FileResponse("front/index.html")
+
+@app.get("/favicon.ico")
+async def main():
+    return FileResponse("front/favicon.ico")
 
 
 class MoveReq(BaseModel):
     direction: str
-    cellNum: int
+    position: int
 
 
-@app.post("/move")
-async def move(move: MoveReq):
-    if move.direction == "right" and move.cellNum + 1 > move.cellNum:
-        return {"Message": f"You can move cellNum = {move.cellNum + 1}"}
+@app.post("/move", status_code=200)
+async def move_func(move: MoveReq, response: Response):
+    if move.direction == "right" and move.position < 3:
+        return {"position": move.position + 1}
+    elif move.direction == "left" and move.position > 1:
+        return {"position": move.position - 1}
     else:
-        return {"Status": 500, "Message": "Wrong move"}
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
