b0934a3120da1b681b120ec21ca2f88f8188ad64
NikitaPRV <nikitaprv@oxygen>
2024-10-02 21:01:39 +0300

    added set_player_position to Player methods

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from typing import Union, Tuple
-
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from starlette.staticfiles import StaticFiles
-from fastapi import status
 from map import Map
 from player import Player
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="front"), name="static")
 
 main_map = Map(7, 7)
@@ -19,57 +15,20 @@ main_map.print_map()
 main_player = Player(main_map.start_y, main_map.start_x)
 
 
-def set_player_position(game_map: Map, player: Player, direction: str = None, response: Response = None):
-    x, y = 0, 0
-    final = game_map.final
-    game_map = game_map.map
-    match direction:
-        case "right":
-            x = 1
-        case "left":
-            x = -1
-        case "up":
-            y = -1
-        case "down":
-            y = 1
-        case _:
-            pass
-
-    try:
-        if game_map[player.y + y][player.x + x] != 0:
-            if player.y + y != -1 and player.x + x != -1:
-
-                game_map[player.y][player.x] = 1
-                player.y += y
-                player.x += x
-                game_map[player.y][player.x] = 2
-
-                if player.y == final[0] and player.x == final[1]:
-                    return game_map, True
-                else:
-                    return game_map, False
-
-        else:
-            response.status_code = status.HTTP_204_NO_CONTENT
-            return response
-    except IndexError:
-        pass
-
-
 @app.get("/")
 async def home():
     return FileResponse("front/index.html")
 
 
-@app.get("/map")
-async def return_map():
-    set_player_position(main_map, main_player)
-    return {"map": main_map.map}
-
-
 @app.get("/favicon.ico")
 async def main():
     return FileResponse("front/favicon.png")
+
+
+@app.get("/map")
+async def return_map() -> dict:
+    main_player.set_player_position(main_map)
+    return {"map": main_map.map}
 
 
 class MoveReq(BaseModel):
@@ -79,7 +38,7 @@ class MoveReq(BaseModel):
 @app.post("/move", status_code=200)
 async def move_func(move: MoveReq, response: Response):
     try:
-        game_map, completed = set_player_position(main_map, main_player, move.direction, response)
+        game_map, completed = main_player.set_player_position(main_map, move.direction, response)
 
         if completed:
             return {"map": game_map, "complete": 1}
--- player.py
+++ player.py
@@ -1,8 +1,55 @@
+from typing import Any, Union
+from fastapi import status
+from map import Map
+from fastapi import Response
+
+
 class Player:
 
     def __init__(self, y: int, x: int):
         self.y = y
         self.x = x
 
-    def get_player_position(self):
+    def get_player_position(self) -> dict:
         return {"y": self.y, "x": self.x}
+
+    def set_player_position(self, game_map: Map, direction: str = None, response: Response = None) -> (Union[
+                                                                                                           Any, bool] |
+                                                                                                       Response | None):
+        x, y = 0, 0
+        final = game_map.final
+        game_map = game_map.map
+
+        match direction:
+            case "right":
+                x = 1
+            case "left":
+                x = -1
+            case "up":
+                y = -1
+            case "down":
+                y = 1
+            case _:
+                pass
+
+        try:
+            if game_map[self.y + y][self.x + x] != 0:
+                if self.y + y != -1 and self.x + x != -1:
+
+                    game_map[self.y][self.x] = 1
+                    self.y += y
+                    self.x += x
+                    game_map[self.y][self.x] = 2
+
+                    if self.y == final[0] and self.x == final[1]:
+                        return game_map, True
+                    else:
+                        return game_map, False
+
+            else:
+                response.status_code = status.HTTP_204_NO_CONTENT
+                return response
+        except IndexError:
+            pass
+        except Exception:
+            pass
