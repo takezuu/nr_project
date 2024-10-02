fd45e3904d6d2f91502b48a43c69de82903dfc02
RomanBTG <romanbtg@oxygen>
2024-10-02 22:14:28 +0300

    PlayerPosition

--- front/script.js
+++ front/script.js
@@ -1,14 +1,16 @@
 const gameContainer = document.getElementById('game-container');
 const winer = document.getElementById('win');
 const page = document.querySelector('body');
+let playerPosition = { x: 0, y: 0 }; 
+let main_map;
 
 // Функция для создания и отображения карты
-function renderMap(gameMap) {
+function renderMap() {
     gameContainer.innerHTML = '';  // Очищаем контейнер
-	gameContainer.style.gridTemplateColumns = `repeat(${gameMap.length}, 50px)`;
-	gameContainer.style.gridTemplateRows = `repeat(${gameMap[0].length}, 50px)`;
+	gameContainer.style.gridTemplateColumns = `repeat(${main_map.length}, 50px)`;
+	gameContainer.style.gridTemplateRows = `repeat(${main_map[0].length}, 50px)`;
 
-    gameMap.forEach(row => {
+    main_map.forEach(row => {
         row.forEach(cell => {
             const cellDiv = document.createElement('div');
             cellDiv.classList.add('cell');
@@ -28,30 +30,40 @@ function renderMap(gameMap) {
 			}
 			
             gameContainer.appendChild(cellDiv);
+       
         });
     });
+}
+
+function movePlayer(newPosition)
+{
+	//index=row×num_columns+column
+	var old_pos = playerPosition.y * main_map.length + playerPosition.x;
+	var new_pos = newPosition.y *  main_map.length + newPosition.x;
+	gameContainer[indx];
 }
 
 // Получение карты с сервера
 async function getMap() {
     const response = await fetch('/map');
     const data = await response.json();
+	main_map = data.map;
 	renderMap(data.map);
 	
 }
 
 // Отправка направления для перемещения игрока
-async function sendMoveRequest(direction) {
+async function sendMoveRequest() {
     const response = await fetch('/move', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ direction: direction })
+        body: JSON.stringify({ playerPosition: playerPosition })
     });
 
     const data = await response.json();
-    renderMap(data.map);  // Обновляем карту с новыми позициями
+    renderMap(data.map);// Обновляем карту с новыми позициями
 	if (data.complete == 1)
 	{
 		page.style.backgroundImage = "url('static/bg2.jpg')"
@@ -59,19 +71,22 @@ async function sendMoveRequest(direction) {
 		gameContainer.style.display = "none";
 		winer.style.display = "contents";
 	}
+	//movePlayer(data.playerPosition);
+	renderMap();
 }
 
 // Добавляем прослушивание нажатий клавиш для перемещения
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight') {
-        sendMoveRequest('right');
+		playerPosition.x+=1;
     } else if (event.key === 'ArrowLeft') {
-        sendMoveRequest('left');
+        playerPosition.x-=1;
     } else if (event.key === 'ArrowUp') {
-        sendMoveRequest('up');
+        playerPosition.y+=1;
     } else if (event.key === 'ArrowDown') {
-        sendMoveRequest('down');
+        playerPosition.y-=1;
     }
+	sendMoveRequest();
 });
 
 // Инициализируем карту при загрузке страницы
