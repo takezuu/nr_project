7eeefe26b13740a11cc6321451902572f91d2e76
NikitaPRV <nikitaprv@oxygen>
2024-10-03 22:52:50 +0300

    changed limit for path

--- map.py
+++ map.py
@@ -39,14 +39,14 @@ class Map:
         self.map[self.y][self.x] = 1
 
         path_length = 0
-        while path_length < 500000:
+        while path_length < 200:
             loop_flag = True
             while loop_flag:
                 path_length += 1
                 moves = self.find_available_moves()
                 loop_flag = self.select_move(moves)
 
-                if loop_flag is False and path_length < 500000:
+                if loop_flag is False and path_length < 200:
                     path_length = 0
                     self.create_empty_map()
                     self.generate_start_position()
